53db3742ec8c9178021750a6ea1793a3a1b30c4d
Sachidananda12 <sachidanadasahoo60@gmail.com>
2025-09-09 14:46:42 +0530

    home Page Images add

--- ecomproject/settings.py
+++ ecomproject/settings.py
@@ -127,8 +127,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
+# if you have a project-level "static" folder
+import os
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+# for collectstatic (deployment)
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
